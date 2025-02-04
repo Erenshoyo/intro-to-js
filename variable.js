74f7735311778641904b0dde80e08accf139f94c
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-02-05 00:04:46 +0600

    Intro to JS

---
+++ variable.js
@@ -0,0 +1,14 @@
+/*
+ * VARIABLE ==> vary + able
+ * 5 things to declare a variable
+ */
+var weight = 38; // Keyword + name = value ;
+var price = 38;
+var year = 2001;
+var money = 2005;
+
+price = 500;
+
+var age;
+console.log(age);
+console.log(price);
