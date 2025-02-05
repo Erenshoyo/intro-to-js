e553d41c566f5d12c93a26ac0cb65044deb410ed
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-02-05 19:13:30 +0600

    Intro to JS

---
+++ naming.js
@@ -0,0 +1,15 @@
+/*********************************************************************
+ *                      Naming in Variables
+ *********************************************************************
+*/
+
+// 1. no keyword in variable name.
+// 2. no space or gap in variable name.
+// 3. no quotation
+// 4. cannot start with a number, but number other the first letter is allowed.
+// 5. name is case sensitive.
+// 6. How to write a long variable name? -->
+
+var myCurrentHomeAddress = "Tongi, Gazipur";// camel case
+var my_current_home_address = "Tongi, Gazipur"; // snake case
+var MyCurrentHomeAddress = "Tongi, Gazipur"; //pascal case
---
+++ number.js
@@ -0,0 +1,7 @@
+var orangePrice = 20; //Int.
+var chocolate = 0.5; //Float
+var applePrice = '20';
+
+console.log(orangePrice + applePrice);
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
